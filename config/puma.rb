dd23a0d62b4c80e9329ebc9e64012278ef21f1df
dominic <dominic@hubblestudios.com>
2016-05-04 10:55:52 +0200

    Add fallback method

--- config/puma.rb
+++ config/puma.rb
@@ -12,6 +12,7 @@ environment ENV['RAILS_ENV'] || 'development'
 
 on_worker_boot do
   require "active_record"
+	#   ActiveRecord::Base.establish_connection
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[Rails.env])
 end
